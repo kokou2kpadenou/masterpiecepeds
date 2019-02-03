26cad5ed68cdc94cac1a9b2b3e8ccff7bfbebcf1
kokou kpadenou <kkokou@gmail.com>
2019-02-02 21:54:17 -0500

    First phase

---
+++ js/script.js
@@ -0,0 +1,281 @@
+var langDict = {
+	'en': {
+		// index.html
+		// header
+		'main-header-slogan': 'Pediatric & Adolescent Medecine',
+		'main-header-btnappointment': 'MAKE AN APPOINTMENT',
+		// service
+		'main-services': 'Services',
+		'main-services-prenatal': 'Prenatal visits',
+		'main-services-prenatal-desc': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+		'main-services-breastfeeding': 'Breastfeeding Advise & Support',
+		'main-services-breastfeeding-desc': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+		'main-services-exams': 'Well-check Exams',
+		'main-services-exams-desc': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+		'main-services-sport': 'Sports Physicals',
+		'main-services-sport-desc': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+		'main-services-adhd': 'ADD/ADHD Consultation',
+		'main-services-adhd-desc': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+		'main-services-sameday': 'Same Day Emergency',
+		'main-services-sameday-desc': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+		// emergency
+		'main-emergency': 'Emergency & After Hours Calls',
+		'main-emergency-text1': 'There is always a physician available and on call 24 hours a day, seven days a week for emergency assistance and after hours advice.',
+		'main-emergency-text2': 'For after hours assistance, please call the number below to contact the on-call physician.',
+		//team
+		'main-team-title': 'Head Doctor, Pediatrics',
+		'main-team-desc': 'Dr Veronica C. Omenka, MD is a practicing Pediatrician in Montgomery Village, MD. Dr. Omenka graduated from Lagos State University College of Medicine in 1992 and has been in practice for 27 years. She completed a residency at Maimonides Medical Center.',
+		'main-team-quote': 'Our highest priority is you, your comfort, your well-being, your total health!',
+		'main-team-nurse': 'We have a high quality, dedicated, qualify and talented nurses team.',
+		// affiliation
+		'main-affiliation': 'Hospital Affiliation',
+		// Office hours
+		'main-office': 'Office Hours',
+		'main-office-text': 'We recommend regularly scheduled preventive healthcare visits during which we monitor the child as a whole: growth and development, safety and nutrition, immunization and encourage preventive health measures.',
+		'main-office-elt1': 'Monday - Thursday',
+		'main-office-elt2': 'Friday',
+		'main-office-elt3': 'Closed for lunch',
+		'main-office-note': 'Weekends by Appointment Only',
+		// make appointment
+		'main-appointment': 'Make Appointment',
+		'main-appointment-fullname': 'Fullname',
+		'main-appointment-phone': 'Phone Number',
+		'main-appointment-date': 'Date',
+		'main-appointment-object': 'Object',
+		'main-appointment-btn': 'Sent Request',
+		'main-appointment-note': 'If you are unable to keep your appointment, please give 24 heures notice.',
+		// testimonials
+		'main-testimonials': 'Testimonials',
+		'main-testimonials-quote1': 'ne of the best physician I have interacted with. Very nice, accommodating and knows what she is doing.',
+		'main-testimonials-title1': 'Front-End Developer',
+		'main-testimonials-quote2': 'Taking my kids to a padiatric has never been easier. They just loved the welcoming and warm atmosphere in there!',
+		'main-testimonials-title2': 'Contractor',
+		'main-testimonials-quote3': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+		'main-testimonials-title3': 'CEO',
+		// contacts
+		'main-contacts': 'Contacts',
+		'main-contacts-text': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+		'main-contacts-phone': 'Phone Number',
+		'main-contacts-email': 'Email',
+		'main-contacts-address': 'Address',
+		'': '',
+		'': '',
+		// footer
+		'main-footer-text1': 'Welcome to Middlebrook Pediatrics, your comprehensive center for pediatric and adolescent care. We provide child-focused, excellent medical care to children from birth to 21 years of age.',
+		'main-footer-text2': 'Our services include: well child care, immunizations, consultations, emergency services and care for acute and chronic illnesses.',
+		'main-footer-office': 'Office',
+		'main-footer-office-line1': 'Monday - Thursday: 9:00 AM - 5:00 PM',
+		'main-footer-office-line2': 'Friday: 9:00 AM - 1:00 PM',
+		'main-footer-office-line3': 'We are closed for lunch from 12:30 PM - 1:30 PM',
+		'main-footer-office-line4': 'Weekends by Appointment Only',
+		'main-footer-links': 'Quick Links',
+		'main-footer-links-team': 'Team',
+		'main-footer-links-affiliation': 'Affiliation',
+		'main-footer-links-testimonials': 'Testimonials',
+		'main-footer-links-contacts': 'Contacts',
+		'menu-home': 'Home',
+		'menu-services': 'Services',
+		'menu-emergency': 'Emergency',
+		'menu-appointment': 'Appointment',
+		'menu-testimonials': 'Testimonials',
+		'menu-contacts': 'Contacts',
+		'menu-ourteam': 'Our team',
+		'menu-officehours': 'Office hours',
+		'global-terms': 'Terms and Conditions',
+		'global-privacy': 'Privacy Policy',
+		'global-copy': '© 2019 Middlebrook Pediatrics. All rights reserved.',
+		'global-home': 'Home',
+	},
+	'es': {
+		'main-header-slogan': 'Medicina pediátrica y adolescente',
+		'main-header-btnappointment': 'CONCERTAR CITA',
+		'main-services': 'Servicios',
+		'main-services-prenatal': 'Visitas prenatales',
+		'main-services-prenatal-desc': 'es Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+		'main-services-breastfeeding': 'Asesoramiento y apoyo a la lactancia materna',
+		'main-services-breastfeeding-desc': 'es Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+		'main-services-exams': 'Examen de bienestar',
+		'main-services-exams-desc': 'es Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+		'main-services-sport': 'Exámenes físicos para el deporte.',
+		'main-services-sport-desc': 'es Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+		'main-services-adhd': 'Consulta ADD / ADHD',
+		'main-services-adhd-desc': 'es Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+		'main-services-sameday': 'Emergencia el mismo día',
+		'main-services-sameday-desc': 'es Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+		'main-emergency': 'Llamadas de emergencia y fuera de horario.',
+		'main-emergency-text1': 'Siempre hay un médico disponible y de guardia las 24 horas del día, los siete días de la semana para asistencia de emergencia y asesoramiento después del horario de atención.',
+		'main-emergency-text2': 'Para obtener asistencia fuera del horario de atención, llame al número que se encuentra a continuación para comunicarse con el médico de guardia.',
+		'main-team-title': 'Director Médico, Pediatría',
+		'main-team-desc': 'Dr Veronica C. Omenka, MD es un pediatra en ejercicio en Montgomery Village, MD. El Dr. Omenka se graduó de la Facultad de Medicina de la Universidad Estatal de Lagos en 1992 y ha estado en práctica durante 27 años. Ella completó una residencia en el Centro Médico Maimonides.',
+		'main-team-quote': '¡Nuestra máxima prioridad es usted, su comodidad, su bienestar, su salud total!',
+		'main-team-nurse': 'Contamos con un equipo de enfermeras de alta calidad, dedicados, calificados y talentosos.',
+		'main-affiliation': 'Afiliación hospitalaria',
+		'main-office': 'Horas de oficina',
+		'main-office-text': 'Recomendamos visitas de atención médica preventivas programadas regularmente durante las cuales monitoreamos al niño en general: crecimiento y desarrollo, seguridad y nutrición, inmunización y fomentamos medidas preventivas de salud.',
+		'main-office-elt1': 'Lunes jueves',
+		'main-office-elt2': 'viernes',
+		'main-office-elt3': 'Cerrado durante el almuerzo',
+		'main-office-note': 'Fines de semana solo con cita',
+		'main-appointment': 'Concertar cita',
+		'main-appointment-fullname': 'Nombre completo',
+		'main-appointment-phone': 'Número de teléfono',
+		'main-appointment-date': 'Fecha',
+		'main-appointment-object': 'Objeto',
+		'main-appointment-btn': 'Enviar solicitud',
+		'main-appointment-note': 'Si no puede asistir a su cita, por favor envíe un aviso de 24 horas.',
+		'main-testimonials': 'Testimonios',
+		'main-testimonials-quote1': 'Uno de los mejores médicos con los que he interactuado. Muy agradable, complaciente y sabe lo que está haciendo.',
+		'main-testimonials-title1': 'Desarrollador Front-End',
+		'main-testimonials-quote2': 'Llevar a mis hijos a una pediatría nunca ha sido tan fácil. ¡A ellos les encantó el ambiente cálido y acogedor de allí!',
+		'main-testimonials-title2': 'Contratista',
+		'main-testimonials-quote3': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+		'main-testimonials-title3': 'CEO',
+		'main-contacts': 'Contactos',
+		'main-contacts-text': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+		'main-contacts-phone': 'Número de teléfono',
+		'main-contacts-email': 'correo electrónico',
+		'main-contacts-address': 'Dirección',
+		'main-footer-text1': 'Bienvenido a Middlebrook Pediatrics, su centro integral de atención pediátrica y adolescente. Ofrecemos atención médica excelente y enfocada en el niño a niños desde el nacimiento hasta los 21 años de edad.',
+		'main-footer-text2': 'Nuestros servicios incluyen: cuidado de niños sanos, inmunizaciones, consultas, servicios de emergencia y atención para enfermedades agudas y crónicas.',
+		'main-footer-office': 'Oficina',
+		'main-footer-office-line1': 'Lunes a jueves de 9:00 a.m. a 5:00 p.m.',
+		'main-footer-office-line2': 'Viernes: 9:00 AM - 1:00 PM',
+		'main-footer-office-line3': 'Estamos cerrados para el almuerzo de 12:30 PM - 1:30 PM',
+		'main-footer-office-line4': 'Fines de semana solo con cita',
+		'main-footer-links': 'enlaces rápidos',
+		'main-footer-links-team': 'Equipo',
+		'main-footer-links-affiliation': 'Afiliación',
+		'main-footer-links-testimonials': 'Testimonios',
+		'main-footer-links-contacts': 'Contactos',
+
+		'menu-home': 'Casa',
+		'menu-services': 'Servicios',
+		'menu-emergency': 'Emergencia',
+		'menu-appointment': 'Cita',
+		'menu-testimonials': 'Testimonios',
+		'menu-contacts': 'Contactos',
+		'menu-ourteam': 'Nuestro equipo',
+		'menu-officehours': 'Horas de oficina',
+		'global-terms': 'Términos y Condiciones',
+		'global-privacy': 'Política de privacidad',
+		'global-copy': '© 2019 Middlebrook Pediatrics. Todos los derechos reservados.',
+		'global-home': 'Casa',
+	}
+};
+
+
+onload  = start;
+
+function start() {
+	var slideshowIntervalId;
+	var slideshowNavBtn = document.getElementsByClassName('slideshow__nav__btn');
+	var curr = 1;
+
+	function Move() {
+		curr = (curr % 4) + 1;
+		document.getElementById('ind' + curr).checked = true;
+	}
+
+	if (slideshowNavBtn.length > 0) {
+
+		for (var i = 0; i < slideshowNavBtn.length; i++) {
+			slideshowNavBtn[i].addEventListener('click', function() {
+				clearInterval(slideshowIntervalId);
+				curr = this.getAttribute('data-id');
+				slideshowIntervalId = setInterval(Move, 10000);
+			});
+		}
+
+		slideshowIntervalId = setInterval(Move, 10000);
+	}
+
+	if ('localStorage' in window) {
+		var local = localStorage.getItem('uiLang');
+		if (local) {
+			defaultLang = local;
+		} else {
+			//Find user agent Language
+			var language = window.navigator.userLanguage || window.navigator.language;
+			if (language.slice(0, 2) === 'es') {
+				defaultLang = 'es';
+				if ('localStorage' in window) {
+					localStorage.setItem('uiLang', defaultLang);
+				}
+			}
+		}
+	}
+
+	if (defaultLang === 'es') {
+		switchLang('es');
+	}
+
+}
+
+
+
+// Close the menu when a mene element is clicked
+var navigationToggle = document.getElementById('navigation-toggle');
+var navigationItem = document.getElementsByClassName('menu-item');
+for (var i = 0; i < navigationItem.length; i++) {
+	navigationItem[i].addEventListener('click', function() {
+		navigationToggle.checked = false;
+	});
+}
+
+
+// Close popup menu when click outside the popup
+var popup = document.getElementsByClassName('popup');
+for (var j=0; j < popup.length; j++) {
+	popup[j].addEventListener('click', function(e) {
+		if (e.target.className === 'popup') {
+			window.location.hash = 'services';
+		}
+	});
+}
+
+
+
+// switch language functionality
+var defaultLang = 'en';
+var btnEn = document.getElementById('en');
+var btnEs = document.getElementById('es');
+var btnLang = document.getElementsByClassName('switch-lang');
+var text = document.getElementsByClassName('translate');
+
+
+
+// Click on the switch language buttons
+for (var i = 0; i < btnLang.length; i++) {
+  btnLang[i].addEventListener('click', function(e) {
+
+		e.preventDefault();
+		if (this.classList.contains('btn--enabled')) {
+			defaultLang = (this.getAttribute('id') === 'es') ? 'es' : 'en';
+			switchLang(defaultLang);
+		}
+
+	});
+}
+
+function switchLang(lang) {
+	//Set document lang
+	document.getElementsByTagName('html')[0].setAttribute('lang', lang);
+	// Switch the button on UI
+	btnEn.classList.toggle('btn--disabled');
+	btnEn.classList.toggle('btn--enabled');
+	btnEs.classList.toggle('btn--disabled');
+	btnEs.classList.toggle('btn--enabled');
+	// Store the language value in the local storage if it is supported
+	if ('localStorage' in window) {
+		localStorage.setItem('uiLang', lang);
+	}
+	// Start the changing the language on UI
+	translation(lang);
+}
+
+
+function translation(lang) {
+	for (var i = 0; i < text.length; i++) {
+		text[i].textContent = langDict[lang][text[i].getAttribute('data-key')];
+	}
+}
