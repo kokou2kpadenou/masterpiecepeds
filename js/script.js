3858ecdd1354388d6ea0ab34c6320ec568f6b69e
kokou kpadenou <kkokou@gmail.com>
2019-08-05 19:47:02 -0400

    update the javascript

--- js/script.js
+++ js/script.js
@@ -1,21 +1,7 @@
 // Others variables
-var confirmation = document.querySelector('.confirmation');
-var confCancel = document.getElementById('conf-cancel');
-var confEdit = document.getElementById('conf-edit');
-var confConfirm = document.getElementById('conf-confirm');
-var comfirmationMsg1 = document.querySelector('.confirmation__msg--1');
-var comfirmationMsg2 = document.querySelector('.confirmation__msg--2');
-var aptFullname = document.getElementById('fullname');
-var aptPhone = document.getElementById('phone');
-var aptDate = document.getElementById('date');
-var aptObject = document.getElementById('object');
-var confData = document.getElementsByClassName('confirmation__data');
-var customCal = document.getElementsByClassName('flatpickr');
-var navigationToggle = document.getElementById('navigation-toggle');
-var navigationItem = document.getElementsByClassName('menu-item');
-var popup = document.getElementsByClassName('popup-close');
 
-var formApt = document.getElementById('form-apt');
+
+var customCal = document.getElementsByClassName('flatpickr');
 var submitBtn = document.getElementById('submit');
 var validationMessage = {
 	fullname: 'Name required.',
@@ -33,13 +19,11 @@ var validationMessage = {
 */
 var allImage = document.querySelectorAll('.easy-loading');
 var headerTarget = document.querySelector('header');
+var testimonials = document.getElementById('testimonials');
 
 var observer = new IntersectionObserver(function(entries, observer) {
   entries.forEach(function (entry) {
     if (!entry.isIntersecting) {
-
-			// flatpickr calendar initialisation
-			customCal.flatpickr(cal);
 
 			// Easy load images
 			for (var i = 0; i < allImage.length; i++) {
@@ -47,11 +31,88 @@ var observer = new IntersectionObserver(function(entries, observer) {
 					allImage[i].setAttribute('src',  allImage[i].getAttribute('data-img'));
 				}
 			}
+
+			// Active background image for testimonials
+			testimonials.classList.add('visible');
+
     }
   })
 }, {});
 
 observer.observe(headerTarget);
+
+var formApt = document.getElementById('form-apt');
+var formObserver = new IntersectionObserver(function(entries, observer) {
+  entries.forEach(function (entry) {
+    if (entry.isIntersecting) {
+
+			/*
+			-----------------------------------------------------------------------------
+			flatpickr props for english
+			-----------------------------------------------------------------------------
+			*/
+
+			if (defaultLang === 'en') {
+
+
+	      var cal = {
+	        enableTime: true,
+	        altFormat: "F j, Y",
+	        dateFormat: 'l, F j, Y H:i',
+	        minDate: new Date().fp_incr(5),
+	        minTime: '09:00',
+	        maxTime: '17:00',
+	        disable: [
+	          function(date) {
+	            return (date.getDay() === 0 || date.getDay() === 6);
+	          }
+	        ],
+	        locale: flatpickr.l10ns.en,
+	        plugins: [new confirmDatePlugin({})],
+	        wrap: true,
+	      };
+
+			}
+
+			/*
+			-----------------------------------------------------------------------------
+			flatpickr props for english
+			-----------------------------------------------------------------------------
+			*/
+
+			if (defaultLang === 'es') {
+
+
+	      var cal = {
+	    		enableTime: true,
+	    		altFormat: "F j, Y",
+	    		dateFormat: 'l, F j, Y H:i',
+	    		minDate: new Date().fp_incr(5),
+	    		minTime: '09:00',
+	    		maxTime: '17:00',
+	    		time_24hr: true,
+	    		disable: [
+	    			function(date) {
+	    				return (date.getDay() === 0 || date.getDay() === 6);
+	    			}
+	    		],
+	    		locale: flatpickr.l10ns.es,
+	    		plugins: [new confirmDatePlugin({})],
+	    		wrap: true,
+	    	};
+			}
+
+			// flatpickr calendar initialisation
+			customCal.flatpickr(cal);
+
+    }
+  })
+}, {});
+
+formObserver.observe(formApt);
+
+
+
 
 
 /*
@@ -86,6 +147,8 @@ function start(e) {
 		slideshowIntervalId = setInterval(Move, 10000);
 
 	}
+
+	others();
 }
 
 
@@ -96,27 +159,52 @@ function start(e) {
 */
 
 
-// Close the menu when a menu element is clicked
-function navToggleClickHandle() {
-	navigationToggle.checked = false;
-}
+function others() {
+	var navigationItem = document.getElementsByClassName('menu-item');
+	var popup = document.getElementsByClassName('popup-close');
+	var navigationToggle = document.getElementById('navigation-toggle');
 
-for (var i = 0; i < navigationItem.length; i++) {
-	navigationItem[i].addEventListener('click', navToggleClickHandle);
-}
-
-
-// Close popup menu when click outside the popup
-function closePopupHandle(e) {
-	if (e.target.classList.contains('popup-close')) {
-		window.location.hash = 'services';
+	// Close the menu when a menu element is clicked
+	function navToggleClickHandle() {
+		navigationToggle.checked = false;
 	}
+
+	for (var i = 0; i < navigationItem.length; i++) {
+		navigationItem[i].addEventListener('click', navToggleClickHandle);
+	}
+
+
+	// Close popup menu when click outside the popup
+	function closePopupHandle(e) {
+		if (e.target.classList.contains('popup-close')) {
+			window.location.hash = 'services';
+		}
+	}
+
+	for (var j=0; j < popup.length; j++) {
+		popup[j].addEventListener('click', closePopupHandle);
+	}
+
 }
 
-for (var j=0; j < popup.length; j++) {
-	popup[j].addEventListener('click', closePopupHandle);
-}
+/*
+	-----------------------------------------------------------------------------
+		Form related
+	-----------------------------------------------------------------------------
+*/
 
+
+var confCancel = document.getElementById('conf-cancel');
+var confEdit = document.getElementById('conf-edit');
+var confConfirm = document.getElementById('conf-confirm');
+var confirmation = document.querySelector('.confirmation');
+var comfirmationMsg1 = document.querySelector('.confirmation__msg--1');
+var comfirmationMsg2 = document.querySelector('.confirmation__msg--2');
+var confData = document.getElementsByClassName('confirmation__data');
+var aptFullname = document.getElementById('fullname');
+var aptPhone = document.getElementById('phone');
+var aptDate = document.getElementById('date');
+var aptObject = document.getElementById('object');
 
 /*
 	-----------------------------------------------------------------------------
@@ -124,16 +212,38 @@ for (var j=0; j < popup.length; j++) {
 	-----------------------------------------------------------------------------
 */
 
-function submitHandle(e) {
-	e.preventDefault();
-	confirmation.classList.toggle('show');
-	fillData();
-	showMsg(1);
+// Reset form
+function aptResetAll() {
+	grecaptcha.reset();
+	submitBtn.disabled = true;
+	formApt.reset();
 }
 
-if (formApt) {
-	formApt.addEventListener('submit', submitHandle);
-};
+// messages
+function showMsg(msg) {
+	if (msg === 1) {
+		comfirmationMsg1.classList.remove('confirmation__invisible');
+		comfirmationMsg2.classList.add('confirmation__invisible');
+		confCancel.classList.remove('confirmation__invisible');
+		confEdit.classList.remove('confirmation__invisible');
+		confConfirm.innerText = 'Confirm';
+		confirmation.removeEventListener('click', formConfHanlde);
+
+	} else {
+		comfirmationMsg1.classList.add('confirmation__invisible');
+		comfirmationMsg2.classList.remove('confirmation__invisible');
+		confCancel.classList.add('confirmation__invisible');
+		confEdit.classList.add('confirmation__invisible');
+		confConfirm.innerText = 'Ok';
+		confirmation.addEventListener('click', formConfHanlde);
+
+	}
+}
+
+// Set focus on Name
+function setFocusFormName() {
+	aptFullname.focus();
+}
 
 
 // fill Data
@@ -158,80 +268,49 @@ function fillData() {
 	}
 }
 
-
 function formConfHanlde(e) {
 	if (e.target.classList.contains('confirmation')) {
 		confirmation.classList.toggle('show');
 	}
 }
 
-
-// messages
-function showMsg(msg) {
-	if (msg === 1) {
-		comfirmationMsg1.classList.remove('confirmation__invisible');
-		comfirmationMsg2.classList.add('confirmation__invisible');
-		confCancel.classList.remove('confirmation__invisible');
-		confEdit.classList.remove('confirmation__invisible');
-		confConfirm.innerText = 'Confirm';
-		confirmation.removeEventListener('click', formConfHanlde);
-
-	} else {
-		comfirmationMsg1.classList.add('confirmation__invisible');
-		comfirmationMsg2.classList.remove('confirmation__invisible');
-		confCancel.classList.add('confirmation__invisible');
-		confEdit.classList.add('confirmation__invisible');
-		confConfirm.innerText = 'Ok';
-		confirmation.addEventListener('click', formConfHanlde);
-
-	}
+function submitHandle(e) {
+	e.preventDefault();
+	confirmation.classList.toggle('show');
+	fillData();
+	showMsg(1);
 }
 
+formApt.addEventListener('submit', submitHandle);
 
-// Reset form
-function aptResetAll() {
+confCancel.addEventListener('click', function() {
+	aptResetAll();
+	confirmation.classList.toggle('show');
+	setFocusFormName();
+});
+
+
+confEdit.addEventListener('click', function() {
+	setFocusFormName();
 	grecaptcha.reset();
 	submitBtn.disabled = true;
-	formApt.reset();
-}
+	confirmation.classList.toggle('show');
+});
 
-// Set focus on Name
-function setFocusFormName() {
-	aptFullname.focus();
-}
-
-// Cancel button click
-if (confCancel) {
-	confCancel.addEventListener('click', function() {
-		aptResetAll();
-		confirmation.classList.toggle('show');
-		setFocusFormName();
-	});
-}
-
-// Edit button click
-if (confEdit) {
-	confEdit.addEventListener('click', function() {
-		setFocusFormName();
-		grecaptcha.reset();
-		submitBtn.disabled = true;
-		confirmation.classList.toggle('show');
-	});
-}
 
 // Confirmation button click
-if (confConfirm) {
-	confConfirm.addEventListener('click', function() {
-		if (this.innerText === 'Confirm') {
-			showMsg(2);
-			// TODO: send email
+confConfirm.addEventListener('click', function() {
+	if (this.innerText === 'Confirm') {
+		showMsg(2);
+		// TODO: send email
 
-			aptResetAll();
-		} else {
-			confirmation.classList.toggle('show');
-		}
-	});
-}
+		aptResetAll();
+	} else {
+		confirmation.classList.toggle('show');
+	}
+});
+
+
 
 
 /*
@@ -240,12 +319,12 @@ if (confConfirm) {
 	-----------------------------------------------------------------------------
 */
 
-
 // Hook Input handle function to the  control
 var inputs = document.getElementsByClassName('appointment__section__input');
 for (var i = 0; i < inputs.length; i++) {
 	inputs[i].addEventListener('input', handleInput);
 }
+
 
 // Event Input handle function
 function handleInput(e) {
